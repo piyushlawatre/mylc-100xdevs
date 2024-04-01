51acf89e57f0a5114cb64a9cb257d1f56cfb4c9f
piyushlawatre <piyushlawatre.dev@gmail.com>
2024-04-01 18:50:20 +0530

    testing agin

--- authentication-jwt (week-3)/app.js
+++ authentication-jwt (week-3)/app.js
@@ -17,7 +17,6 @@ app.get("/", (req, res, next) => {
 
 app.post("/sign-up", (req, res, next) => {
   try {
-    //ZOD
     const User = z.object({
       email: z.string().email(),
       password: z.string().max(6).max(12),
