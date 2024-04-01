bb5fecaf258744d9cf047e3d05f7983c4c46e998
Piyush Lawatre <136806904+piyushlawatre@users.noreply.github.com>
2024-04-01 12:40:20 +0000

    adding zod

--- authentication-jwt (week-3)/app.js
+++ authentication-jwt (week-3)/app.js
@@ -1,9 +1,34 @@
-import express from "express"
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import z from "zod";
 dotenv.config();
 
-const app = express()
+const app = express();
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server Started At Port ${process.env.PORT}`)
-})
+app.use((req, res, next) => {
+  console.log("Request Logger");
+  console.log(`${req.method} : ${req.url}`);
+  next();
+});
+
+app.get("/", (req, res, next) => {
+  res.json({ result: "Function: bound parserOnIncoming" });
+});
+
+app.post("/sign-up", (req, res, next) => {
+  try {
+    const User = z.object({
+      email: z.string().email(),
+      password: z.string().max(6).max(12),
+    });
+    const user = req.body;
+    User.parse(user);
+    z.string().email();
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server Started At Port ${process.env.PORT}`);
+});
